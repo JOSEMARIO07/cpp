1d3f37055c6016d8e4683ca90222964110b3f201
Mario Enamorado <enamoradojosemario1@gmail.com>
2023-10-18 07:47:28 -0600

    agregando ejercicio funciones

---
+++ Ejercicios/Ejercicio25-Suma de pares e impares y total/suma de pares e impares.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+    int total
+	int sumapar=0;
+	sumaimpar=0;
+	
+	for(int  i=1;i<=10;i++){
+		if(i%2 !=0){
+		sumaimpar=sumaimpar+i;
+		}
+		else
+			sumapar=i+sumapar;
+			total=pares+impares;
+    }			
+	cout << "la suma de los pares es: " << sumapar<<endl;
+	cout << "la suma de los impares es: " <<sumaimpar<<endl;
+	cout << "la suma de los numeros pares e impares es: "<< total<<endl;
+	return 0;
+}
---
+++ Ejercicios/Ejercicio28-Funciones/funciones.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int sumar() {
+    return 5 + 7;
+{
+    string nombreCompleto () {
+        string nomre = "Jose";
+        string apellido = "Enamorado";
+
+        return nombre + " " + apellido;
+        
+}
+
+int main(int argc, char const *argv[])
+{
+    int resultado = sumar ();
+    string nombreyApellido = nombreCompleto();
+
+        cout << resultado;
+
+        cout << endl;
+
+        cout << nombreyApllido;
+
+        return 0;
+}
